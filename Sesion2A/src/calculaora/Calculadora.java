39482deedf27c4b21e6263e42927a396510d0121
israe <israe@Raeliss>
2024-10-04 10:18:30 +0200

    creation

---
+++ Sesion2A/src/calculaora/Calculadora.java
@@ -0,0 +1,19 @@
+package calculaora;
+
+public class Calculadora {
+
+	public float suma(int a, int b) {
+		return a+b;
+	}
+	
+	public float resta(int a, int b) {
+		return a-b;
+	}
+	public float divide(int a, int b) {
+		return a/b;
+	}
+	public float multiplica(int a, int b) {
+		return a*b;
+	}
+	
+}
